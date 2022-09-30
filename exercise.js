efa41599a267258c8d8c7e60c771630fe510fa7b
Jacinto De la Hoz ITK <113387069+JacintoDelahoz-Ksquare@users.noreply.github.com>
2022-09-30 16:44:17 -0400

    Second commit

--- exercise.js
+++ exercise.js
@@ -9,12 +9,18 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const spinal = (str) => {
     /* Only make changes below this comment */
+    let separatewhenthereisnospace = str.replace(/([a-z])([A-Z])/g, "$1 $2")
+    let twobigcasestogether = separatewhenthereisnospace.replace(/([A-Z])([A-Z])/g, "$1 $2")
+    let spaceforadash = twobigcasestogether.replace(/\s/g, "-")
     
-    
+    // This take every work and change it to lowercase
+    return spaceforadash.toLowerCase();
+
     /* Only make changes below this comment */
 }
 
-
+let A = spinal('ThisIsATest');
+console.log(A);
 
 
 
