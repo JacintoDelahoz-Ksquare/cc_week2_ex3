ca100d954041424ce56bb75d6555e1c7df0bd1f4
Jacinto De la Hoz ITK <113387069+JacintoDelahoz-Ksquare@users.noreply.github.com>
2022-10-06 13:30:26 -0400

    Second commit

--- exercise.js
+++ exercise.js
@@ -9,9 +9,11 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const spinal = (str) => {
     /* Only make changes below this comment */
-    let separatewhenthereisnospace = str.replace(/([a-z])([A-Z])/g, "$1 $2")
-    let twobigcasestogether = separatewhenthereisnospace.replace(/([A-Z])([A-Z])/g, "$1 $2")
-    let spaceforadash = twobigcasestogether.replace(/\s/g, "-")
+
+    let separatewhenthereisnospace = str.replace(/([a-z])([A-Z])/g, "$1 $2");
+    let separatewhenthereistwocappitalsnexttoeachother = separatewhenthereisnospace.replace(/([A-Z])([A-Z])/g, "$1 $2");
+    let spaceforadash = separatewhenthereistwocappitalsnexttoeachother.replace(/\s|_/g, "-");
+
     
     // This take every work and change it to lowercase
     return spaceforadash.toLowerCase();
